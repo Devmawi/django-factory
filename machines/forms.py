86c831a70db23459935446b219ecad0f15849a31
Dev <dev@dev.dev>
2021-06-06 17:14:32 +0200

    add hints

--- machines/forms.py
+++ machines/forms.py
@@ -1,6 +1,7 @@
 from django.forms import ModelForm
 from .models import Machine
 
+# more on https://docs.djangoproject.com/en/3.2/topics/forms/modelforms/#overriding-the-default-fields
 class MachineForm(ModelForm):
     class Meta:
          model = Machine
